eec2df47446c33b9eb707e81cff8a066b309beed
Maciej Witkowiak <mwitkowiak@gmail.com>
2020-07-04 22:09:06 +0100

    local test setup

--- insmodule.sh
+++ insmodule.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-KVERSION=`uname -r`
-mkdir -p /lib/modules/${KVERSION}/extra
-sudo cp gpiots.ko /lib/modules/${KVERSION}/extra/
-sudo insmod /lib/modules/${KVERSION}/extra/gpiots.ko gpios=17,22
+##KVERSION=`uname -r`
+##mkdir -p /lib/modules/${KVERSION}/extra
+##sudo cp gpiots.ko /lib/modules/${KVERSION}/extra/
+##sudo insmod /lib/modules/${KVERSION}/extra/gpiots.ko gpios=17,22
+sudo insmod ./gpiots.ko gpios=17,22 gpio_lp_button=27 gpio_odd_even=23
