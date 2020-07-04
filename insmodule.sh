a618e4189eb83509ae9f6bdfb008470673fcf999
Maciej Witkowiak <mwitkowiak@gmail.com>
2020-07-05 00:34:13 +0100

    reload module

--- insmodule.sh
+++ insmodule.sh
@@ -3,4 +3,5 @@
 ##mkdir -p /lib/modules/${KVERSION}/extra
 ##sudo cp gpiots.ko /lib/modules/${KVERSION}/extra/
 ##sudo insmod /lib/modules/${KVERSION}/extra/gpiots.ko gpios=17,22
+sudo rmmod gpiots
 sudo insmod ./gpiots.ko gpios=17,22 gpio_lp_button=27 gpio_odd_even=23
